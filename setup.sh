23ff4235b7dc051706ae4511bbdd22bfc5a0c216
Kevin Johnson <kej.johnson@gmail.com>
2016-05-19 18:13:57 -0400

    I forgot what I did

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd ~/dotfiles
-ln -s bashrc ~/.bashrc
-ln -s bash_profile ~/.bash_profile
-ln -s init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
